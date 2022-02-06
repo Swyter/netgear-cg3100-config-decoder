4a28c214d2fb24a174361dad81b920bf49e54adc
Swyter <swyterzone@gmail.com>
2022-02-06 12:51:35 +0100

    Compute the salted MD5 hash and verify the validity of the bundled checksum. I was sure it had to be MD5, but many thanks to Joseph Lehner for bcm2-utils and the salt value (the extra bytes one needs to append for the checksums to match).
    	md5(b_dec[16:] + b"\x32\x50\x73\x6c\x63\x3b\x75\x28\x65\x67\x6d\x64\x30\x2d\x27\x78")

--- netgear-cg3100-config-decoder.py
+++ netgear-cg3100-config-decoder.py
@@ -1,10 +1,13 @@
 from ast import arguments
 import os, sys
+import hashlib
 
-filename="GatewaySettings(4).bin.dec"
+filename="GatewaySettings(4).bin"
 
 if len(sys.argv) > 1:
     filename = sys.argv[1]
+
+print(f'[i] opening «{filename}»')
 
 text=""
 b=None
@@ -19,12 +22,14 @@ with open(f"{filename}", "rb") as f:
     #        text += "--\n"
 #print(text)
 
-encode = False
-suffix = ".dec"
-
 if filename.endswith(".dec"):
+    print(f'[+] encoding')
     encode = True
     suffix = ".bin"
+else:
+    print(f'[+] decoding')
+    encode = False
+    suffix = ".dec"
     
 b_dec = bytearray(b)
 
@@ -53,3 +58,9 @@ with open(f"{filename}{suffix}", "wb") as fout:
                 b_dec[i + 1] = tmp_a
 
         fout.write(int.to_bytes(b_dec[i], 1, "little"))
+
+if not encode:
+    # swy: thanks to Joseph Lehner for all the work; see gwsettings.cc and FORMAT.md in bcm2-utils
+    ret = hashlib.md5(b_dec[16:] + b"\x32\x50\x73\x6c\x63\x3b\x75\x28\x65\x67\x6d\x64\x30\x2d\x27\x78")
+    print(f"[i] existing MD5 hash: {b_dec[:16].hex()}", ret.digest() == b_dec[:16] and "(valid)" or "(invalid)" )
+    print(f"[i] computed MD5 hash: {ret.digest().hex()}")
