fc957e49d9c864b02248f111388b4e9074f852b6
Swyter <swyterzone@gmail.com>
2022-02-07 07:09:52 +0100

    Improve the script comments with some extra thoughts.

--- netgear-cg3100-config-decoder.py
+++ netgear-cg3100-config-decoder.py
@@ -37,7 +37,7 @@ with open(f"{filename}{suffix}", "wb") as fout:
     for i, byte in enumerate(b):
         if (i & 1) == 0:
             if encode:
-                # swy: swap
+                # swy: swap it back
                 tmp_a = b_dec[i    ]
                 tmp_b = b_dec[i + 1]
                 b_dec[i    ] = tmp_b
@@ -47,11 +47,12 @@ with open(f"{filename}{suffix}", "wb") as fout:
                 #      make it wrap around
                 b_dec[i] = (b_dec[i] + i) & 0xff
             else:
-                # swy: subtract the odd bytes against their file offset
-                #      make it wrap around
+                # swy: to decode; subtract the odd bytes against
+                #      their file offset and make it wrap around
                 b_dec[i] = (b_dec[i] - i) & 0xff
 
-                # swy: swap
+                # swy: swap; wtfbbq, maybe an artifact of the endianness
+                #      for the ushort-sized blocks ¯\_(ツ)_/¯
                 tmp_a = b_dec[i    ]
                 tmp_b = b_dec[i + 1]
                 b_dec[i    ] = tmp_b
@@ -63,7 +64,7 @@ if not encode:
     stated_size = struct.unpack(">I", b_dec[0x5c:0x60])[0]
     print(f'[i] stated size: {stated_size:#x}, actual size: {len(b_dec[16:]):#x}')
 
-    # swy: thanks to Joseph Lehner for all the work; see gwsettings.cc and FORMAT.md in bcm2-utils
+    # swy: thanks to Joseph Lehner for all the work and this generic salt value; see gwsettings.cc and FORMAT.md in bcm2-utils
     ret = hashlib.md5(b_dec[16:16 + stated_size] + b"\x32\x50\x73\x6c\x63\x3b\x75\x28\x65\x67\x6d\x64\x30\x2d\x27\x78")
     print(f'[i] computed MD5 hash: {ret.digest().hex()}')
     print(f'[i] existing MD5 hash: {  b_dec[:16].hex()}', ret.digest() == b_dec[:16] and '(valid)' or '(invalid)')
